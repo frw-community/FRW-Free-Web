f3bcd9b86243832167a0f8aaeb07bf30973ebd83
deepload <n.chappuis@outlook.com>
2025-11-15 00:40:23 +0100

    Fix normal

--- apps/browser/vite.config.ts
+++ apps/browser/vite.config.ts
@@ -1,45 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
-import renderer from 'vite-plugin-electron-renderer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    electron([
-      {
-        entry: 'src/main/index.ts',
-        vite: {
-          build: {
-            outDir: 'dist/main',
-            rollupOptions: {
-              external: [
-                'ipfs-http-client',
-                'electron',
-                '@frw/common',
-                '@frw/crypto',
-                '@frw/ipfs',
-                '@frw/protocol'
-              ]
-            }
-          }
-        }
-      },
-      {
-        entry: 'src/preload/index.ts',
-        vite: {
-          build: {
-            outDir: 'dist/preload',
-            rollupOptions: {
-              external: ['electron']
-            }
-          }
-        }
-      }
-    ]),
-    renderer()
-  ],
+  plugins: [react()],
   build: {
-    outDir: 'dist/renderer'
+    outDir: 'dist-electron',
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173
   }
 });
